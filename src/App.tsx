34e87f900ddeac722011e57cc5a370bfc4a94d78
ziadseleem <ziad32seleem@gmail.com>
2026-02-19 01:08:14 +0200

    add initial React application setup including core components (Navbar), 3D models, textures, and various UI assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import './App.css'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import Navbar from './sections/Navbar'
 
-function RotatingCube() {
-  const meshRef = useRef<any>(null)
-
-  useFrame((_state, delta) => {
-    meshRef.current.rotation.x += delta * 0.5
-    meshRef.current.rotation.y += delta * 0.5
-  })
-
-  return (  
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={0x9cdba6} emissive={"#9cdba6"}/>
-    </mesh>
-  )
-}
-
-function App() {
-  
+const App = () => {
   return (
-    <Canvas className='flex justify-center items-center' style={{height: '100vh', width: '100vw'}}>
-      <OrbitControls enableZoom enablePan enableRotate />
-      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
-
-      <color attach="background" args={['#696969']} />
-      <RotatingCube />
-    </Canvas>
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+    </main>
   )
 }
 
