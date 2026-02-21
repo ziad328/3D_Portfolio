cc734a330823acd85d1cd2403eb65315ee5b6958
ziadseleem <ziad32seleem@gmail.com>
2026-02-21 16:58:22 +0200

    feat: Implement About section with globe visualization and set up it in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Hero from './sections/Hero'
 import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import About from './sections/About'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar />
       <Hero />
+      <About />
     </main>
   )
 }
