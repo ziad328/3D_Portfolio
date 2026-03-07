660c01779cda5bc574588e40ac06199d50445329
ziadseleem <ziad32seleem@gmail.com>
2026-03-07 04:15:28 +0200

    feat: add Footer component and integrate it into the main application layout.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './sections/About'
 import Projects from './sections/Projects'
 import Clients from './sections/Clients'
 import Contact from './sections/Contact'
+import Footer from './sections/Footer'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Projects />
       <Clients />
       <Contact />
+      <Footer />
     </main>
   )
 }
